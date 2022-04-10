3ab616e416c5faca687e84fb3cd0dbd5023ed02a
bamcerr <bamcerr@gmail.com>
2022-04-10 22:11:26 +0900

    add typescript, setup apollo client

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
+import { ApolloProvider } from "@apollo/client"
+import client from './apollo';
+
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
