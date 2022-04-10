5324dc534b745b6f5228d14685ad83308eef8ecd
bamcerr <bamcerr@gmail.com>
2022-04-11 08:18:13 +0900

    add query

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -1,5 +1,29 @@
+import { useParams } from "react-router";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIE = gql`
+  query getMovie($id: ID!) {
+    movie(id: $id) {
+      id,
+      title,
+      medium_cover_image,
+      description_full
+    }
+  }
+`
+
 export default () => {
-  return (
-    <div>Detail</div>
-  )
+  const { id } = useParams();
+
+  const { loading, data, error } = useQuery(GET_MOVIE, {
+    variables: { id }
+  })
+
+  if(loading) {
+    return "loading";
+  }
+
+  if(data && data.movie) {
+    return data.movie.title;
+  }
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,27 @@
+import { useQuery, gql } from "@apollo/client";
+import { Link } from "react-router-dom";
+
+const GET_MOVIES = gql`
+{
+  movies {
+    id
+    medium_cover_image
+  }
+}
+`
+
+interface Movie {
+  id: number,
+  medium_cover_image: string
+}
+
 export default () => {
-  return (
-    <div>Home</div>
-  )
+  const { loading, data, error } = useQuery(GET_MOVIES)
+
+  return (<>
+    <div>
+      {loading && <div>Loding...</div>}
+      {!loading && data?.movies.map((movie: Movie) => { return <div key={movie.id}><Link to={`${movie.id}`}>{movie.id}</Link></div> })}
+    </div>
+  </>)
 }
